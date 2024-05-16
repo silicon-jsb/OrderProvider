d2e82b3afbd058826b49059cb5f981977a657a3b
Hanna Grehn <hannagrehn@gmail.com>
2024-05-16 17:12:35 +0200

    here

--- Data/Entities/OrderCourse.cs
+++ Data/Entities/OrderCourse.cs
@@ -4,9 +4,10 @@ namespace Data.Entities;
 public class SavedCourses
 {
 	//same as in other project 
+
 	public string FirstName { get; set; } = null!;
 	public string LastName { get; set; } = null!;
 	public string User {  get; set; }
 	public int OrderId { get; set; }
-
+	//here
 }
