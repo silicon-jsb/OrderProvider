67bb47bb3df39d0795af932be045b0159d8f05f3
Hanna Grehn <hannagrehn@gmail.com>
2024-05-21 11:27:06 +0200

    common

---
+++ InsertDummy.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('Manda', 101, 'Course Title 1');
+
+INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('Manda', 102, 'Course Title 2');
+
+INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('Panda', 103, 'Course Title 3');
+
+INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('Panda', 104, 'Course Title 4');
+
+INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('Panda', 104, 'Course Title 5');
+
+INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('Jolanda', 104, 'Course Title 6');
+
+INSERT INTO SavedCourses (UserId, CourseId, CourseTitle)
+VALUES ('JoeKenda', 104, 'Course Title 7');
