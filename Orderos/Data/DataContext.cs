f0401f5ff2c3263efd448b585345f2af0ddbd4b3
Hanna Grehn <hannagrehn@gmail.com>
2024-05-28 12:28:07 +0200

    good

--- Orderos/Data/DataContext.cs
+++ Orderos/Data/DataContext.cs
@@ -7,4 +7,9 @@ namespace Orderos.Data;
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
 	public DbSet<SavedCoursesEntity> SavedCourses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-silicon-jsb.database.windows.net,1433;Initial Catalog=orders_newdatabase;Persist Security Info=False;User ID=SqlAdmin;Password=Bytmig123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+    }
 }
